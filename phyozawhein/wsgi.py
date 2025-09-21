37de246eee0885c1c711b745b848d3bb796775c1
NoadDD-01 <noahdd216@gmail.com>
2025-09-21 16:19:06 +0000

    update

--- phyozawhein/wsgi.py
+++ phyozawhein/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PHyoZAwHEIn.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'phyozawhein.settings')
 
 application = get_wsgi_application()
